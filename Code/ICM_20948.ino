c72af271f8ac96334294b296519739377f647a03
dfedor2008 <dawid.fedor08@gmail.com>
2025-07-08 09:49:09 -0400

    Update ICM_20948.ino

--- Code/ICM_20948.ino
+++ Code/ICM_20948.ino
@@ -5,6 +5,11 @@ void ICM20948() {
     float x_accel = myICM.accX();
     float y_accel = myICM.accY();
     float z_accel = myICM.accZ();
+    float x_gyro = myICM.gyrX();
+    float y_gyro = myICM.gyrY();
+    float z_gyro = myICM.gyrZ();
+
+    Serial.println("X_Accel, Y_Accel, Z_Accel");
 
     Serial.print(x_accel);
     Serial.print(", ");
@@ -12,7 +17,13 @@ void ICM20948() {
     Serial.print(", ");
     Serial.println(z_accel);
 
-    File accelFile = SD.open("ACCEL.TXT", FILE_WRITE);
+    Serial.println("X_Gyro, Y_Gyro, Z_Gyro");
+    Serial.print(x_gyro);
+    Serial.print(y_gyro);
+    Serial.print(z_gyro);
+
+
+    File accelFile = SD.open("ICM20498.TXT", FILE_WRITE);
     if (accelFile) {
       accelFile.print("Seconds after Launch  ");
       accelFile.print(sec_after_launch, 2);
@@ -21,10 +32,14 @@ void ICM20948() {
       accelFile.print(", ");
       accelFile.print(y_accel, 2);
       accelFile.print(", ");
-      accelFile.println(z_accel, 2);
+      accelFile.print(z_accel, 2);
+      accelFile.print("        ");
+      accelFile.print(x_gyro, 2);
+      accelFile.print(y_gyro, 2);
+      accelFile.println(z_gyro, 2);
       accelFile.close();
     } else {
-      Serial.println("Failed to open ACCEL.TXT");
+      Serial.println("Failed to open ICM20498.TXT");
     }
   }
 }
