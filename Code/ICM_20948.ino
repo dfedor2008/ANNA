4d67de53283137f8bc7311121811260130dd5fe6
dfedor2008 <dawid.fedor08@gmail.com>
2025-08-02 20:51:47 -0400

    Update ICM_20948.ino

--- Code/ICM_20948.ino
+++ Code/ICM_20948.ino
@@ -5,9 +5,9 @@ void ICM20948() {
     float x_accel = myICM.accX();
     float y_accel = myICM.accY();
     float z_accel = myICM.accZ();
-    float x_gyro = myICM.gyrX();
-    float y_gyro = myICM.gyrY();
-    float z_gyro = myICM.gyrZ();
+    float x_gyro = myICM.gyrX() - gx_offset;
+    float y_gyro = myICM.gyrY() - gy_offset;
+    float z_gyro =myICM.gyrZ() - gz_offset;
 
     Serial.println("X_Accel, Y_Accel, Z_Accel");
 
@@ -22,11 +22,10 @@ void ICM20948() {
     Serial.print(y_gyro);
     Serial.print(z_gyro);
 
-
     File accelFile = SD.open("ICM20498.TXT", FILE_WRITE);
     if (accelFile) {
       accelFile.print("Seconds after Launch  ");
-      accelFile.print(sec_after_launch, 2);
+      accelFile.print(elapsed / 1000);
       accelFile.print("        ");
       accelFile.print(x_accel, 2);
       accelFile.print(", ");
@@ -35,11 +34,32 @@ void ICM20948() {
       accelFile.print(z_accel, 2);
       accelFile.print("        ");
       accelFile.print(x_gyro, 2);
+      accelFile.print(", ");
       accelFile.print(y_gyro, 2);
+      accelFile.print(", ");
       accelFile.println(z_gyro, 2);
       accelFile.close();
     } else {
+      Serial.println();
       Serial.println("Failed to open ICM20498.TXT");
     }
   }
+}
+void calibrateGyro() {
+  Serial.println("Calibrating gyro... Keep the device still.");
+  for (int i = 0; i < samples; i++) {
+    myICM.getAGMT();  // refresh data
+    gx_offset += myICM.gyrX();
+    gy_offset += myICM.gyrY();
+    gz_offset += myICM.gyrZ();
+    delay(2);
+  }
+  gx_offset /= samples;
+  gy_offset /= samples;
+  gz_offset /= samples;
+
+  Serial.print("Gyro offsets: ");
+  Serial.print(gx_offset); Serial.print(", ");
+  Serial.print(gy_offset); Serial.print(", ");
+  Serial.println(gz_offset);
 }
